d5803a2390a81efabf6b56a4e8654260292b43b6
CentOS <yangmiemie@localhost.localdomain>
2015-08-02 01:50:47 +0800

    add kth smallest element in a bst in c

--- c/kth_smallest_element_in_a_bst.c
+++ c/kth_smallest_element_in_a_bst.c
@@ -10,7 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct TreeNode {
+struct TreeNode 
+{
   int val;
   struct TreeNode* left;
   struct TreeNode* right;
@@ -19,23 +20,103 @@ struct TreeNode {
 typedef struct TreeNode* Tree;
 typedef struct TreeNode* Position;
 
-struct TreeNode* invertTree(struct TreeNode* root) {
-    if (root == NULL)
-      return NULL;
+#define ElementType Position
 
-    Tree T;
+struct StackRecord
+{
+  int Size;
+  int Capacity;
+  ElementType* Next;
+};
 
-    T = root -> right;
-    root -> right = root -> left;
-    root -> left = T;
+typedef struct StackRecord* Stack;
+#define SIZE 2
 
-    invertTree(root -> left);
-    invertTree(root -> right);
+Stack Initialize()
+{
+  Stack S;
 
-    return root;
+  S = malloc(sizeof(struct StackRecord));
+  S -> Size = 0;
+  S -> Capacity = SIZE;
+  S -> Next = malloc(sizeof(ElementType) * S -> Capacity);
+
+  return S;
 }
 
+int IsEmpty(Stack S)
+{
+  return S -> Size == 0;
+}
 
+int IsFull(Stack S)
+{
+  return S -> Capacity == S -> Size;
+}
+
+ElementType Pop(Stack S)
+{
+  if (IsEmpty(S))
+  {
+    fprintf(stderr, "Empty Stack\n");
+    exit(1);
+  }
+
+  return S -> Next[--S -> Size];
+}
+
+void Push(Stack S, ElementType X)
+{
+  if (IsFull(S))
+  {
+    S -> Next = realloc(S -> Next, sizeof(ElementType) * S -> Capacity * 2);
+    S -> Capacity *= 2;
+  }
+
+  S -> Next[S -> Size++] = X;
+}
+
+void Destroy(Stack S)
+{
+  free(S -> Next);
+  free(S);
+}
+
+int kthSmallest(struct TreeNode* root, int k) {
+  Stack S;
+  Position Node;
+  int i;
+
+  if (root == NULL)
+    return 0;
+
+  S = Initialize();
+  Node = root;
+  i = 0;
+  while (Node != NULL || !IsEmpty(S))
+  {
+    if (Node != NULL)
+    {
+      Push(S, Node);
+      Node = Node -> left;      
+    }
+    else
+    {
+      Node = Pop(S);
+
+      if (++i == k)
+        break;
+      // printf("%d ", Node -> val);
+
+      if (Node -> right)
+        Node = Node -> right;
+      else
+        Node = NULL;
+    }
+  }
+
+  return Node -> val;
+}
 
 Tree NewNode(int value)
 {
@@ -119,9 +200,27 @@ void MakeEmpty(Tree T)
   return;
 }
 
+// int TestStack()
+// {
+//  Stack S;
+//  int i;
+
+//  S = Initialize();
+//  for (i = 0; i < 100; ++i)
+//    Push(S, i);
+
+//  while (!IsEmpty(S))
+//    printf("%d ", Pop(S));
+
+//  printf("\n");
+
+//  Destroy(S);
+// }
+
 int main(int argc, char const *argv[])
 {
   Tree T;
+  int Value;
 
   T = CreateTree();
 
@@ -134,16 +233,7 @@ int main(int argc, char const *argv[])
   PostOrderTraverse(T);
   printf("\n");
 
-  T = invertTree(T);
-
-  PreOrderTraverse(T);
-  printf("\n");
-
-  InOrderTraverse(T);
-  printf("\n");
-
-  PostOrderTraverse(T);
-  printf("\n");
-
+  Value = kthSmallest(T, 4);
+  printf("Value = %d\n", Value);
   return 0;
 }
